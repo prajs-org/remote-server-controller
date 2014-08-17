97c5abbfd7c2d3efc3427a56cae2b03c555f22a4
Karel Prajs <karel@prajs.org>
2014-08-17 15:31:14 +0000

    refactoring, comments

--- RemoteServerController/RscHost/Program.cs
+++ RemoteServerController/RscHost/Program.cs
@@ -1,17 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
-
-using RscConfig;
-
+﻿/******************************************************************************
+ * Remote Server Controller, http://rsc.codeplex.com                          *
+ *                                                                            *
+ * Copyright (C) 2014 Karel Prajs                                             *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.      *
+ ******************************************************************************/
 namespace RscHost
 {
+    // System namespaces
+    using System;
+    using System.Linq;
+    using System.ServiceProcess;
+
+    // Project namespaces
+    using RscConfig;
+
     static class Program
     {
         /// <summary>
         /// The main entry point for the application.
+        /// Application can be executed as Windows Managed Service or with standalone host.
+        /// Standalone host is not recommended for production usage.
+        /// Use argument configured in constant StandaloneHostFlag to run this application in standalone mode.
         /// </summary>
         static void Main(string[] args)
         {
@@ -19,13 +40,17 @@ namespace RscHost
             var service = new RscService();
 
             // Start service or run it on console for debug
-            if (args.Count() > 0 && args[0] == "/CONSOLE")
+            if (args.Count() > 0 && args[0] == Constants.StandaloneHostFlag)
             {
+                // ----------------------------
+                // Standalone host
+                // ----------------------------
+
                 // Print welcome message
                 Helpers.WelcomeMessage();
 
                 // Start service
-                service.StartForDebug(new string[] { });
+                service.StartManually();
 
                 // Wait on exit command
                 while (true)
@@ -36,10 +61,14 @@ namespace RscHost
                 }
                 
                 // Stop service
-                service.StopForDebug();
+                service.StopManually();
             }
             else
             {
+                // ----------------------------
+                // Windows Managed Service host
+                // ----------------------------
+
                 ServiceBase.Run(service);
             }
         }
--- RemoteServerController/RscHost/RscService.cs
+++ RemoteServerController/RscHost/RscService.cs
@@ -1,19 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
-using System.ServiceModel.Web;
-
-using RscConfig;
-using RscLog;
-using System.Configuration.Install;
-
+﻿/******************************************************************************
+ * Remote Server Controller, http://rsc.codeplex.com                          *
+ *                                                                            *
+ * Copyright (C) 2014 Karel Prajs                                             *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.      *
+ ******************************************************************************/
 namespace RscHost
 {
+    // System namespaces
+    using System;
+    using System.ComponentModel;
+    using System.ServiceProcess;
+    using System.ServiceModel.Web;
+    using System.Configuration.Install;
+
+    // Project namespaces
+    using RscConfig;
+    using RscLog;
+    
+    /// <summary>
+    /// Host class which can be used both for standalone application host and Windows Managed Service host.
+    /// The standalone mode is not recommended for production usage. It is mentioned only for development, debugging and tests.
+    /// Only single WCF host is supported at the moment (do not mix up WCF host and application host).
+    /// </summary>
     public partial class RscService : ServiceBase
     {
         WebServiceHost host = null;
@@ -23,16 +43,31 @@ namespace RscHost
             InitializeComponent();
         }
 
-        public void StartForDebug(string[] args)
+        #region Manual control for standalone host
+
+        /// <summary>
+        /// Start the program manually. Use only for standalone host mode.
+        /// </summary>
+        public void StartManually()
         {
-            OnStart(args);
+            OnStart(new string[]{});
         }
+        /// <summary>
+        /// Stop the program manually. Use only for standalone host mode.
+        /// </summary>
+        public void StopManually()
+        {
+            OnStop();
+        }
+
+        #endregion
+
+        #region Windows Managed Service interface
 
         protected override void OnStart(string[] args)
         {
             try
             {
-                // Create and open host
                 if (Configurator.Settings.Network.UseSSL)
                 {
                     host = RscCore.Endpoints.GetRESTHostSSL();
@@ -46,18 +81,9 @@ namespace RscHost
             }
             catch (Exception ex)
             {
-                Log.Error("General error: " + ex.Message);
-                if (ex.InnerException != null)
-                {
-                    Log.Error(ex.InnerException.Message);
-                }
+                Log.Error(ex, "Host could not been started.");
                 this.OnStop();
             }
-        }
-
-        public void StopForDebug()
-        {
-            OnStop();
         }
 
         protected override void OnStop()
@@ -69,11 +95,16 @@ namespace RscHost
             }
             catch (Exception ex)
             {
-                Log.Error("Host could not been closed: " + ex.Message);
+                Log.Error(ex, "Host could not been closed correctly.");
             }
         }
+
+        #endregion
     }
 
+    /// <summary>
+    /// Class allows easy installation of this application as Windows Managed Service.
+    /// </summary>
     [RunInstaller(true)]
     public class ProjectInstaller : Installer
     {
@@ -85,8 +116,8 @@ namespace RscHost
             process = new ServiceProcessInstaller();
             process.Account = ServiceAccount.LocalSystem;
             service = new ServiceInstaller();
-            service.ServiceName = "RscService";
-            service.DisplayName = "Remote Server Controller";
+            service.ServiceName = Constants.AppName;
+            service.DisplayName = Constants.AppDisplayName;
             service.Description = "Service allows remote users to control this server.";
             Installers.Add(process);
             Installers.Add(service);
