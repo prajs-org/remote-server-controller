dead35b02171c039fee635fc656229ae3f67f6b8
Karel Prajs <karel@prajs.org>
2014-08-17 16:10:21 +0000

    refactoring, comments

--- RemoteServerController/RscInterface/IRESTController.cs
+++ RemoteServerController/RscInterface/IRESTController.cs
@@ -1,38 +1,61 @@
-﻿/*
-Copyright (C) 2014 Karel Prajs
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.ServiceModel;
-using System.ServiceModel.Web;
-
+﻿/******************************************************************************
+ * Remote Server Controller, http://rsc.codeplex.com                          *
+ *                                                                            *
+ * Copyright (C) 2014 Karel Prajs                                             *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.      *
+ ******************************************************************************/
 namespace RscInterface
 {
+    // System namespaces
+    using System.ServiceModel;
+    using System.ServiceModel.Web;
+
+    // Project namespaces
+    // -- none
+
     [ServiceContract]
     public interface IRESTController
     {
+        /// <summary>
+        /// Return current status of given service if allowed by permission configuration.
+        /// See specification for possible action results.
+        /// </summary>
+        /// <param name="serviceName">Service name</param>
+        /// <returns>Object with information about current status and result of this action.</returns>
         [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "JSON/Service/Status/{serviceName}")]
         [OperationContract]
         ServiceStatus ServiceStatusJSON(string serviceName);
-
+        /// <summary>
+        /// Try to start service of given name.
+        /// Success is not guaranteed.
+        /// Always check action result!
+        /// See specification for possible action results.
+        /// </summary>
+        /// <param name="serviceName">Service name</param>
+        /// <returns>Object with information about result of this action.</returns>
         [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "JSON/Service/Start/{serviceName}")]
         [OperationContract]
         ServiceActionResult ServiceStartJSON(string serviceName);
-
+        /// <summary>
+        /// Try to stop service of given name.
+        /// Success is not guaranteed.
+        /// Always check action result!
+        /// See specification for possible action results.
+        /// </summary>
+        /// <param name="serviceName">Service name</param>
+        /// <returns>Object with information about result of this action.</returns>
         [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "JSON/Service/Stop/{serviceName}")]
         [OperationContract]
         ServiceActionResult ServiceStopJSON(string serviceName);
--- RemoteServerController/RscInterface/IRESTDataContracts.cs
+++ RemoteServerController/RscInterface/IRESTDataContracts.cs
@@ -1,41 +1,57 @@
-﻿/*
-Copyright (C) 2014 Karel Prajs
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Runtime.Serialization;
-using System.ServiceProcess;
-
-using RscConfig;
-
+﻿/******************************************************************************
+ * Remote Server Controller, http://rsc.codeplex.com                          *
+ *                                                                            *
+ * Copyright (C) 2014 Karel Prajs                                             *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.      *
+ ******************************************************************************/
 namespace RscInterface
 {
+    // System namespaces
+    using System.Runtime.Serialization;
+    using System.ServiceProcess;
+
+    // Project namespaces
+    using RscConfig;
+
+    /// <summary>
+    /// Data contract for checking of service status.
+    /// </summary>
     [DataContract]
     public class ServiceStatus
     {
+        /// <summary>
+        /// Service current status
+        /// </summary>
         [DataMember]
         public string Status { get; set; }
-
+        /// <summary>
+        /// Service name
+        /// </summary>
         [DataMember]
         public string Name { get; set; }
-
+        /// <summary>
+        /// Result of checking service status
+        /// </summary>
         [DataMember]
         public string Result { get; set; }
-
+        /// <summary>
+        /// Data contract with result of checking of service status.
+        /// </summary>
+        /// <param name="name">Service name</param>
+        /// <param name="status">Current service status</param>
+        /// <param name="result">Result of checking of service status</param>
         public ServiceStatus(string name, ServiceControllerStatus? status, ReturnCodes.ActionReturnCode result)
         {
             this.Name = name;
@@ -43,19 +59,33 @@ namespace RscInterface
             this.Result = result.ToString();
         }
     }
-
+    /// <summary>
+    /// Data contract for changing service status.
+    /// </summary>
     [DataContract]
     public class ServiceActionResult
     {
+        /// <summary>
+        /// Service new status (after change) - does not need to be desired status, always check Result!
+        /// </summary>
         [DataMember]
         public string Status { get; set; }
-
+        /// <summary>
+        /// Service name
+        /// </summary>
         [DataMember]
         public string Name { get; set; }
-
+        /// <summary>
+        /// Result of changing service status
+        /// </summary>
         [DataMember]
         public string Result { get; set; }
-
+        /// <summary>
+        /// Data contract with result of changing of service status.
+        /// </summary>
+        /// <param name="name">Service name</param>
+        /// <param name="status">Service status after change</param>
+        /// <param name="result">Result of changing of service status</param>
         public ServiceActionResult(string name, ServiceControllerStatus? status, ReturnCodes.ActionReturnCode result)
         {
             this.Name = name;
