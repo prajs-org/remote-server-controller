5e7a07e84e28af2dd8ac3870ae78ab2a371bcc3b
prajs-org <karel@prajs.org>
2017-04-04 18:49:57 +0200

    refactoring: updated header files after migration to github

--- RemoteServerController/RscCore/Controllers/ServiceController/ServiceManager.cs
+++ RemoteServerController/RscCore/Controllers/ServiceController/ServiceManager.cs
@@ -1,7 +1,7 @@
 ﻿/******************************************************************************
- * Remote Server Controller, http://rsc.codeplex.com                          *
- *                                                                            *
- * Copyright (C) 2014 Karel Prajs                                             *
+ * Remote Server Controller                                                   *
+ * https://github.com/prajs-org/remote-server-controller                      *
+ * Copyright (C) 2014-217 Karel Prajs, karel@prajs.org                        *
  *                                                                            *
  * This program is free software: you can redistribute it and/or modify       *
  * it under the terms of the GNU General Public License as published by       *
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License          *
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.      *
  ******************************************************************************/
+
 namespace RscCore.Controllers.ServiceController
 {
     // System namespaces
