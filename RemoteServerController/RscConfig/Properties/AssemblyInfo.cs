7f57fe89306a06efda9032e4a32603cad5674b8b
prajs-org <karel@prajs.org>
2017-06-05 18:09:24 +0200

    refactoring: licence headers fix

--- RemoteServerController/RscConfig/Properties/AssemblyInfo.cs
+++ RemoteServerController/RscConfig/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿/******************************************************************************
  * Remote Server Controller                                                   *
  * https://github.com/prajs-org/remote-server-controller                      *
- * Copyright (C) 2014-217 Karel Prajs, karel@prajs.org                        *
+ * Copyright (C) 2014-2017 Karel Prajs, karel@prajs.org                        *
  *                                                                            *
  * This program is free software: you can redistribute it and/or modify       *
  * it under the terms of the GNU General Public License as published by       *
